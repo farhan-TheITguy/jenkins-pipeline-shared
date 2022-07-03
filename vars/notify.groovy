38b38ebb474ce60d35374f01c74ddc1378901d76
Mohd Farhan <85340728+farhan-TheITguy@users.noreply.github.com>
2022-07-03 12:40:06 +0530

    Create notify.groovy

---
+++ vars/notify.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  println "This is comming from shared liberary"
+  println this.args
+}
